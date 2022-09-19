0c832e65ab6b970d624b0e0ace79976186a1e892
MIKE <mikebonus777@gmail.com>
2022-09-19 01:47:06 -0400

    change-made 4 (with onboarding)

--- app/src/main/java/com/example/cbcnewsapi/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cbcnewsapi/presentation/MainActivity.kt
@@ -14,7 +14,7 @@ import com.example.cbcnewsapi.presentation.ui.NewsActivity
 import com.example.cbcnewsapi.presentation.ui.VideoActivity
 import com.example.cbcnewsapi.utils.Extender
 import kotlinx.android.synthetic.main.activity_main.*
-// change made one.... another pull request 3
+// change made one.... another pull request 4
 class MainActivity : AppCompatActivity() {
 
     val TAG = "TAG"
